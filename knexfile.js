47b9285d86538e0c3506ce4d146ba4b65b9539ae
Nuranggi Hermawan <nuranggihermawan@gmail.com>
2022-08-14 10:33:59 +0700

    Update

--- knexfile.js
+++ knexfile.js
@@ -4,11 +4,11 @@ module.exports = {
   development: {
     client: "mysql",
     connection: {
-      host : '127.0.0.1',
-      port : 3306,
-      user : 'root',
-      password : '',
-      database : 'DB_DOCUMENT_DEV'
+      host : process.env.DB_HOST,
+      port : process.env.DB_PORT,
+      user : process.env.DB_USERNAME,
+      password : process.env.DB_PASSWORD,
+      database : process.env.DB_NAME
     },
     migrations: {
       directory: "./infrastructure/database/migrations",
